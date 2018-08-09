c61e1d03cc467540dd93cde392c20d3897429dc2
kotapiku <ice08235523@gmail.com>
2018-08-09 17:54:17 +0900

    clean

--- atcoder/typical-dp/d.rs
+++ atcoder/typical-dp/d.rs
@@ -4,6 +4,8 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 use std::io::{stdin, stdout, BufWriter, Write};
 use std::iter::{FromIterator, Iterator};
 
+extern crate primal;
+
 macro_rules! max{($x:expr)=>{$x};($x:expr,$($xs:tt)+)=>{max($x,max!($($xs)+))};}
 macro_rules! min{($x:expr)=>{$x};($x:expr,$($xs:tt)+)=>{min($x,min!($($xs)+))};}
 macro_rules! dump{
@@ -15,29 +17,8 @@ macro_rules! dump{
 const INF: i64 = 0x3f3f3f3f3f3f3f3f;
 const MOD: i64 = 1000000007;
 
-fn f(rp: f64, rq: f64) -> f64 {
-    (1.0+10f64.powf((rp-rq)/400.0)).recip()
-}
-
 fn main() {
-    let k:usize = readln();
-    let n = 2usize.pow(k as u32);
-    let vr:Vec<i32> = (0..n).map(|_| readln::<i32>()).collect();
-    let mut dp = vec![vec![0.0; k+1]; n]; // dp[i][j] = iさんがjラウンドで勝つ確率
-
-    for i in 0..n {
-        dp[i][0] = 1.0;
-    }
-
-    for j in 1..k+1 {
-        for i in 0..n {
-            dp[i][j] = dp[i][j-1] * ((0..n).filter(|&p| ((p >> (j-1)) & 1) != ((i >> (j-1)) & 1) && (p >> j == (i >> j)))
-                                           .map(|p| f(vr[p] as f64, vr[i] as f64)*dp[p][j-1])
-                                           .sum::<f64>());
-        }
-    }
-
-    for i in 0..n { println!("{}", dp[i][k]); }
+    let (n:u64, d:u64) = readln();
 }
 
 trait Read {
@@ -99,4 +80,5 @@ impl<A: Read, B: Read, C: Read, D: Read> Read for (A, B, C, D) {
         (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]), D::read(tokens[3]))
     }
 }
-
+macro_rules! max{($x:expr)=>{$x};($x:expr,$($xs:tt)+)=>{max($x,max!($($xs)+))};}
+macro_rules! min{($x:expr)=>{$x};($x:expr,$($xs:tt)+)=>{min($x,min!($($xs)+))};}
