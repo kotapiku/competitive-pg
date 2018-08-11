590785e851bc43a396bdb9da5e28af87f51dae8d
kotapiku <ice08235523@gmail.com>
2018-08-12 01:13:49 +0900

    change name from dump to debug

--- rust-cp/macro.rs
+++ rust-cp/macro.rs
@@ -6,7 +6,7 @@ use std::iter::{FromIterator, Iterator};
 
 macro_rules! max{($x:expr)=>{$x};($x:expr,$($xs:tt)+)=>{max($x,max!($($xs)+))};}
 macro_rules! min{($x:expr)=>{$x};($x:expr,$($xs:tt)+)=>{min($x,min!($($xs)+))};}
-macro_rules! dump{
+macro_rules! debug{
     ($($a:expr),*) => {
         println!(concat!($(stringify!($a), " = {:?}, "),*), $($a),*);
     }
@@ -76,5 +76,3 @@ impl<A: Read, B: Read, C: Read, D: Read> Read for (A, B, C, D) {
         (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]), D::read(tokens[3]))
     }
 }
-macro_rules! max{($x:expr)=>{$x};($x:expr,$($xs:tt)+)=>{max($x,max!($($xs)+))};}
-macro_rules! min{($x:expr)=>{$x};($x:expr,$($xs:tt)+)=>{min($x,min!($($xs)+))};}
