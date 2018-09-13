44d4ec3038abd549d68e9f29168c7cd47eaaf3c1
kotapiku <ice08235523@gmail.com>
2018-09-13 15:10:21 +0900

    solve arc-102c

---
+++ atcoder/arc-102/c.rs
@@ -0,0 +1,80 @@
+#![allow(unused)]
+use std::cmp::{max, min, Ordering};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+use std::io::{stdin, stdout, BufWriter, Write};
+use std::iter::{FromIterator, Iterator};
+
+macro_rules! max{($x:expr)=>{$x};($x:expr,$($xs:tt)+)=>{max($x,max!($($xs)+))};}
+macro_rules! min{($x:expr)=>{$x};($x:expr,$($xs:tt)+)=>{min($x,min!($($xs)+))};}
+macro_rules! dump{
+    ($($a:expr),*) => {
+        println!(concat!($(stringify!($a), " = {:?}, "),*), $($a),*);
+    }
+}
+ 
+const MOD: i64 = 1000000007;
+
+fn main() {
+    let (n, k):(u64, u64) = readln();
+    if k % 2 == 0 && k/2 <= n { println!("{}", (n/k).pow(3) + ((n-(k/2))/k+1).pow(3)); } else { println!("{}", (n/k).pow(3)); }
+}
+
+trait Read {
+    fn read(s: &str) -> Self;
+}
+
+fn readln<T: Read>() -> T {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    T::read(buf.trim_right())
+}
+ 
+macro_rules! read_impl{
+    ($($t:ty)*) => ($(
+        impl Read for $t {
+            fn read(s: &str) -> $t{
+                s.parse().unwrap()
+            }
+        }
+    )*)
+}
+read_impl! { usize u8 u16 u32 u64 isize i8 i16 i32 i64 f32 f64 }
+ 
+impl Read for String {
+    fn read(s: &str) -> Self {
+        s.to_string()
+    }
+}
+ 
+impl Read for Vec<char> {
+    fn read(s: &str) -> Self {
+        s.chars().collect()
+    }
+}
+ 
+impl<T: Read> Read for Vec<T> {
+    fn read(s: &str) -> Self {
+        s.split_whitespace().map(T::read).collect()
+    }
+}
+ 
+impl<A: Read, B: Read> Read for (A, B) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]))
+    }
+}
+ 
+impl<A: Read, B: Read, C: Read> Read for (A, B, C) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]))
+    }
+}
+ 
+impl<A: Read, B: Read, C: Read, D: Read> Read for (A, B, C, D) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]), D::read(tokens[3]))
+    }
+}
