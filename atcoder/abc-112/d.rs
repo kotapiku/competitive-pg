33b23ba918af5f3b89520db8a2c00e19905c2f33
kotapiku <ice08235523@gmail.com>
2018-12-16 19:24:28 +0900

    add abc-112-d

---
+++ atcoder/abc-112/d.rs
@@ -0,0 +1,79 @@
+#![allow(unused)]
+
+fn main() {
+    let (n, m): (i64, i64) = readln();
+
+    let mut ans: i64 = 1;
+    for i in 1..(m as f64).sqrt() as i64 + 1 {
+        if m % i != 0 { continue; } else {
+            if (m/i) * n <= m {
+                ans = std::cmp::max(ans, m/i);
+            }
+            if i*n <= m {
+                ans = std::cmp::max(ans, i);
+            }
+        }
+    }
+    println!("{}", ans);
+
+}
+
+trait Read {
+    fn read(s: &str) -> Self;
+}
+
+fn readln<T: Read>() -> T {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    T::read(buf.trim_right())
+}
+
+macro_rules! read_impl{
+    ($($t:ty)*) => ($(
+        impl Read for $t {
+            fn read(s: &str) -> $t{
+                s.parse().unwrap()
+            }
+        }
+    )*)
+}
+read_impl! { usize u8 u16 u32 u64 isize i8 i16 i32 i64 f32 f64 }
+
+impl Read for String {
+    fn read(s: &str) -> Self {
+        s.to_string()
+    }
+}
+
+impl Read for Vec<char> {
+    fn read(s: &str) -> Self {
+        s.chars().collect()
+    }
+}
+
+impl<T: Read> Read for Vec<T> {
+    fn read(s: &str) -> Self {
+        s.split_whitespace().map(T::read).collect()
+    }
+}
+
+impl<A: Read, B: Read> Read for (A, B) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]))
+    }
+}
+
+impl<A: Read, B: Read, C: Read> Read for (A, B, C) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]))
+    }
+}
+
+impl<A: Read, B: Read, C: Read, D: Read> Read for (A, B, C, D) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]), D::read(tokens[3]))
+    }
+}
