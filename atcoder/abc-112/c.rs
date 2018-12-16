1095c26033f7061402b2781e6c7c1e89dab0bcd8
kotapiku <ice08235523@gmail.com>
2018-12-16 18:49:46 +0900

    add abc-112-c

---
+++ atcoder/abc-112/c.rs
@@ -0,0 +1,78 @@
+#![allow(unused)]
+
+fn main() {
+    let n: i64 = readln();
+    let datas: Vec<(i64, i64, i64)> = (0..n).map(|_| readln()).collect();
+
+    let (x1, y1, h1) = *datas.iter().find(|&&xyh| xyh.2 > 0).unwrap();
+
+    for i in 0..101 {
+        for j in 0..101 {
+            let h = h1 + (i-x1).abs() + (j-y1).abs();
+            if datas.iter().all(|&(xi, yi, hi)| std::cmp::max(h - (i-xi).abs() - (j-yi).abs(), 0) == hi) {
+                println!("{} {} {}", i, j, h);
+                return;
+            }
+        }
+    }
+}
+
+trait Read {
+    fn read(s: &str) -> Self;
+}
+
+fn readln<T: Read>() -> T {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    T::read(buf.trim_right())
+}
+
+macro_rules! read_impl{
+    ($($t:ty)*) => ($(
+        impl Read for $t {
+            fn read(s: &str) -> $t{
+                s.parse().unwrap()
+            }
+        }
+    )*)
+}
+read_impl! { usize u8 u16 u32 u64 isize i8 i16 i32 i64 f32 f64 }
+
+impl Read for String {
+    fn read(s: &str) -> Self {
+        s.to_string()
+    }
+}
+
+impl Read for Vec<char> {
+    fn read(s: &str) -> Self {
+        s.chars().collect()
+    }
+}
+
+impl<T: Read> Read for Vec<T> {
+    fn read(s: &str) -> Self {
+        s.split_whitespace().map(T::read).collect()
+    }
+}
+
+impl<A: Read, B: Read> Read for (A, B) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]))
+    }
+}
+
+impl<A: Read, B: Read, C: Read> Read for (A, B, C) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]))
+    }
+}
+
+impl<A: Read, B: Read, C: Read, D: Read> Read for (A, B, C, D) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]), D::read(tokens[3]))
+    }
+}
